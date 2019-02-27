db36ab4e18fbd0fec3381b856cd3b484d329e690
Rashmi <rashmiap.10@gmail.com>
2019-02-27 12:10:32 +0530

    Added check to see if user exists before signup
    If email already exists in the database then don't allow the duplicate users with same email-id to register in the database.
    Came across your blog - https://appdividend.com/2018/02/07/node-js-jwt-authentication-tutorial-scratch/ 
    Very helpful. Thanks!

--- routes/user.route.js
+++ routes/user.route.js
@@ -7,30 +7,46 @@ const User = require('../models/user.model');
 
 router.post('/signup', function(req, res) {
    console.log(req.body);
-   bcrypt.hash(req.body.password, 10, function(err, hash){
+   User.findOne({ email: req.body.email }, function(err, user) {
       if(err) {
          return res.status(500).json({
             error: err
          });
       }
-      else {
-         const user = new User({
-            _id: new  mongoose.Types.ObjectId(),
-            email: req.body.email,
-            password: hash    
-         });
-         user.save().then(function(result) {
-            console.log(result);
-            res.status(200).json({
-               success: 'New user has been created'
-            });
-         }).catch(error => {
-            res.status(500).json({
-               error: err
-            });
+      // user with email id already exists
+      if (user) {
+         return res.status(400).json({
+            error: 'A user with that email has already registered. Please use a different email..'
          });
       }
-   });
+      // user does not exist 
+      else{
+         bcrypt.hash(req.body.password, 10, function(err, hash){
+            if(err) {
+               return res.status(500).json({
+                  error: err
+               });
+            }
+            else {
+               const user = new User({
+                  _id: new  mongoose.Types.ObjectId(),
+                  email: req.body.email,
+                  password: hash    
+               });
+               user.save().then(function(result) {
+                  console.log(result);
+                  res.status(200).json({
+                     success: 'New user has been created'
+                  });
+               }).catch(error => {
+                  res.status(500).json({
+                     error: err
+                  });
+               });
+            }
+         });
+      }
+   })
 });
 
 router.post('/signin', function(req, res){
